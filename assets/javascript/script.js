6bfadab83a438940ab033f3ad570cf2a832d12f9
Nathanael White <narhanaelw99@gmail.com>
2021-04-13 15:53:28 -0500

    initial pseudocode and application structure

---
+++ assets/javascript/script.js
@@ -0,0 +1,35 @@
+// create variables for questions 
+    // each var has an array of answers a, b, c, d ??? 
+
+// add time constraint for quizQuestions function using setTimeout. lets say 3 min
+
+// create a function for getting the answers. quizQuestion
+// every time a question is asked, it needs to be appended to the main html element
+
+    // create object literals for each question ???
+        // question: ""
+            // answers: {
+                // a: "",
+                // b: "",
+                //......
+            // }
+            // correct answer: ""
+
+    // use if statements questions... if the answer is correct, then score++. if wrong, then deduct time from quiz
+
+// take the answers and add them to localstorage using setItem 
+
+// use an event listener (on click) to take highscore out of localstorage using getItem and show highscore
+
+
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
